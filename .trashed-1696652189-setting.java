3e203b565d0208c6e3f22bb202ce3bfed2aa5caf
bk8818637-star <bk8818637@gmail.com>
2025-09-27 14:11:42 +0530

    Add files via upload

---
+++ .trashed-1696652189-setting.java
@@ -0,0 +1,8 @@
+
+public class setting {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}
---
+++ .trashed-1696652190-Java.java
@@ -0,0 +1,7 @@
+
+public class Jav
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
+}
